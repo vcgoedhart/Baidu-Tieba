73e02dca3c298c73053eb00511c8850dac3c89c0
Vincent <vcgoedhart@hotmail.com>
2019-06-19 10:48:15 +0200

    Framework changed

--- view/templates/header.php
+++ view/templates/header.php
@@ -3,8 +3,19 @@
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Studenten app</title>	
-		<link rel="stylesheet" href="<?= URL ?>">
+		<link rel="stylesheet" href="<?= URL ?>css/style.css">
+
+		<link rel="icon" type="image/png" href="<?= URL ?>template_login/images/icons/favicon.ico"/>
+		<link rel="stylesheet" type="text/css" href="<?= URL ?>template_login/vendor/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="<?= URL ?>template_login/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="<?= URL ?>template_login/vendor/animate/animate.css">
+		<link rel="stylesheet" type="text/css" href="<?= URL ?>template_login/vendor/css-hamburgers/hamburgers.min.css">
+		<link rel="stylesheet" type="text/css" href="<?= URL ?>template_login/vendor/select2/select2.min.css">
+		<link rel="stylesheet" type="text/css" href="<?= URL ?>template_login/css/util.css">
+		<link rel="stylesheet" type="text/css" href="<?= URL ?>template_login/css/main.css">
+		
 	</head>
 	<body>
 		<nav>
