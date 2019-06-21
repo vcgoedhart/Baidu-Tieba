5ce2825df3d7488947f82cf40cad2c1ab9589753
Miguel Kerschkamp <99051637@mydavinci.nl>
2019-06-21 10:56:58 +0200

    login/register

--- public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define("ROOT", dirname(__DIR__) . DIRECTORY_SEPARATOR);
-
-require(ROOT . "core/config.php");
-require(ROOT . "core/route.php");
-require(ROOT . "core/core.php");
-
-route();
